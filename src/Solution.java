c6e16bceb909d36a72f13097aac4c711bb2a8cdf
cristy-mariya <151554556+cristy-mariya@users.noreply.github.com>
2026-04-09 17:42:28 +0530

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,24 @@
 import java.util.*;
 
-public class Solution {
+public class Solution{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         String line = sc.nextLine().trim();
         String[] words = line.split("\\s+");
 
-        // Use TreeMap to auto sort keys
         TreeMap<String, Integer> map = new TreeMap<>();
 
         for (String word : words) {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
 
+        // Print in ONE LINE
+        StringBuilder sb = new StringBuilder();
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+            sb.append(entry.getKey()).append(": ").append(entry.getValue()).append(" ");
         }
+
+        System.out.print(sb.toString().trim());
     }
 }
