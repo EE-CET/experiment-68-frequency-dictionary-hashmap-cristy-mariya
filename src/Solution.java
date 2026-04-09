1454ed7df883c0de20809a92e8440d80ef388337
cristy-mariya <151554556+cristy-mariya@users.noreply.github.com>
2026-04-09 17:36:33 +0530

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,30 +1,20 @@
 import java.util.*;
 
-public class Solution {
+public class Solution{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Read full line
-        String line = sc.nextLine();
+        String line = sc.nextLine().trim();  // remove extra spaces
+        String[] words = line.split("\\s+"); // handle multiple spaces
 
-        // Split into words
-        String[] words = line.split(" ");
-
-        // Create HashMap
         HashMap<String, Integer> map = new HashMap<>();
 
-        // Count frequency
         for (String word : words) {
-            if (map.containsKey(word)) {
-                map.put(word, map.get(word) + 1);
-            } else {
-                map.put(word, 1);
-            }
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
 
-        // Print result
-        for (String key : map.keySet()) {
-            System.out.println(key + ": " + map.get(key));
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 }
